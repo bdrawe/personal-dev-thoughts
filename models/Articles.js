18aa4de178da3af92520009176ff25a53719eca0
B. Drawe <brycerdrawe.b@gmail.com>
2021-02-23 21:58:57 -0700

    various veiws and route set up

---
+++ models/Articles.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    title:{
+        required: true,
+        type: String
+    },
+    description: {
+        type: String
+    }, 
+    markdown: {
+        required: true,
+        type: String
+    }, 
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model("Article",articleSchema)
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require('express');
+const mongoose = require("mongoose")
 const app = express();
+const Article = require("./models/Articles")
 const articleRouter = require('./routes/articles')
 
-app.set('view engine', 'ejs')
-app.use('/articles', articleRouter)
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: "Testing123",
-        createdAt: Date.now(),
-        description: "Test Description"
-    },
-    {
-        title: "New Blog Post",
-        createdAt: Date.now(),
-        description: "Test Description Again"
-    }
-    ]
-    res.render('index', {articles: articles})
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
+
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async(req, res) => {
+   const articles = await Article.find().sort({ createdAt: 'desc'})
+    res.render('articles/index', {articles: articles})
+})
+app.use('/articles', articleRouter)
 
 app.listen(5000)
