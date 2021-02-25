38a366bb04d4120296ed3300d16f85e7da2ecc7f
B. Drawe <brycerdrawe.b@gmail.com>
2021-02-24 22:22:04 -0700

    sluggify and delete routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require("mongoose")
-const app = express();
 const Article = require("./models/Articles")
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express();
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost/blog', {
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
 app.get('/', async(req, res) => {
    const articles = await Article.find().sort({ createdAt: 'desc'})
